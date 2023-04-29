de6207fe515c7313f5443a546afdbd525c0ca21a
steve <serosenstein@gmail.com>
2023-04-28 20:56:05 -0600

    made advanced search a popup

--- style.php
+++ style.php
@@ -240,3 +240,45 @@ border-radius: 4px;
 box-sizing: border-box;
 }
 
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content {
+  background-color: #fefefe;
+  margin: auto;
+  padding: 0px;
+  /*border: 1px solid #888;*/
+  width: 80%;
+  border-radius: 5px;
+  background-color: grey;
+}
+
+/* The Close Button */
+.close {
+  color: #FFF;
+  /*color: #aaaaaa;*/
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  /*color: #000;*/
+  color: #FFF;
+  text-decoration: none;
+  cursor: pointer;
+}
